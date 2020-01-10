b3eab55caed90dbc41f0af5899424695b1555e5f
Oleksandra Akulshyna <oleksandraakulshyna@Oleksandras-MacBook-Pro.local>
2020-01-10 15:23:45 +0100

    Added heroku config

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+app.enable('trust proxy');
+
 app
   .set('view engine', 'pug')
   .set('views', path.join(__dirname, 'views'))
